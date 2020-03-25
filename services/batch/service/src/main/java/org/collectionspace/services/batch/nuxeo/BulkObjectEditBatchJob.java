51ef1dae126f2ceeb02b51682c9e2a69a43a04b8
cesarvh <cesarv.h@berkeley.edu>
2020-03-25 15:13:12 -0700

    CC-1058: Refactored code

--- services/batch/service/src/main/java/org/collectionspace/services/batch/nuxeo/BulkObjectEditBatchJob.java
+++ services/batch/service/src/main/java/org/collectionspace/services/batch/nuxeo/BulkObjectEditBatchJob.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +16,14 @@ import org.collectionspace.services.common.ResourceMap;
 import org.collectionspace.services.common.NuxeoBasedResource;
 
 import org.collectionspace.services.client.CollectionObjectClient;
+import org.collectionspace.services.client.PayloadOutputPart;
 import org.collectionspace.services.client.PoxPayloadOut;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.Node;
 
 public class BulkObjectEditBatchJob extends  AbstractBatchJob {
   final Logger logger = LoggerFactory.getLogger(BulkObjectEditBatchJob.class);
@@ -39,27 +46,36 @@ public class BulkObjectEditBatchJob extends  AbstractBatchJob {
         csids.addAll(ctx.getListCSIDs().getCsid());
         HashMap<String, String>  fieldsToValues = this.getValues();
         InvocationResults results = new InvocationResults();
-        
 
         if (fieldsToValues.isEmpty()) {
           throw new Exception("There is nothing to update. Aborting...");
         }
-        
+
         // setResults(updateRecords(csids, fieldsToValues));
-        
         int NumAffected = 0 ;
         String payload = preparePayload(fieldsToValues);
+        PoxPayloadOut batchPayload = new PoxPayloadOut(payload.getBytes());
+
+        
+        // PoxPayloadOut collectionObjectPayload = findCollectionObjectByCsid(collectionObjectCsid);
 
         for (String csid : csids) {
-          if (updateRecord(csid, payload) != -1) {
-            NumAffected += 1;
-          } else {
-            // Make this more obvious?
-            logger.warn("The recordw ith csid " +  csid + " was not updated");
+          String mergedPayload = mergePayloads(csid, batchPayload);
+
+          if (mergedPayload != null) {
+            if(updateRecord(csid, mergedPayload) != -1) {
+              NumAffected += 1;
+            } else {
+              // Make this more obvious?
+              logger.warn("The record with csid " +  csid + " was not updated");
+            }
           }
         }
         setCompletionStatus(STATUS_COMPLETE);
+        results.setNumAffected(numAffected);
+        results.setUserNote("reindexed " + numAffected + " records");
 
+        setResults(results);
       } else {
         throw new Exception("Unsupported invocation mode " + mode);
       }
@@ -72,15 +88,6 @@ public class BulkObjectEditBatchJob extends  AbstractBatchJob {
     /* Values that will require special handing:
       pahma: inventoryCount, pahmaFieldLocVerbatim, pahmaEthnographicFileCodeList
       ucbnh naturalhistory: taxon
-
-      groups: We may have to merge these...
-        - objectProductionPlaceGroupList > objectProductionPlaceGroup > objectProductionPlace
-        - objectProductionPersonGroupList > objectProductionPersonGrou > objectProductionPerson
-        - assocPeopleGroupList > assocPeopleGroup > assocPeople
-        - taxonomicIdentGroupList > taxonomicIdentGroup > taxon
-        - materialGroupList > materialGroup > material
-        
-      
     
     */ 
 
@@ -91,25 +98,60 @@ public class BulkObjectEditBatchJob extends  AbstractBatchJob {
     Boolean pahma = false;
     Boolean ucbnh = false;
 
+    String otherNumber = "<otherNumberList><otherNumber>";
+    Boolean otherNumFlag = false;
+
     for (String key : fieldsToUpdate.keySet()) {
       String value = fieldsToUpdate.get(key);
-      if (key.equals("objectProductionPlaceGroup"))  {
-        commonValues += "<objectProductionPlaceGroupList><" + key + ">" + value + "<" + key + "/><objectProductionPlaceGroupList/>";
-      } else if (key.equals("objectProductionPersonGroup")) {
-        commonValues += "<objectProductionPersonGroupList><" + key + ">" + value + "<" + key + "/><objectProductionPersonGroupList/>";
-      } else if (key.equals("assocPeopleGroup")) {
-        commonValues += "<assocPeopleGroupList><" + key + ">" + value + "<" + key + "/><assocPeopleGroupList/>";
-      } else if (key.equals("taxonomicIdentGroupList")) {
-        ucbnhValues += "<taxonomicIdentGroup><" + key + ">" + value + "<" + key + "/><taxonomicIdentGroup/>";
-        ucbnh = true;
-      } else if (key.equals("materialGroup")) {
-        commonValues += "<materialGroupList><" + key + ">" + value + "<" + key + "/><materialGroupList/>";
-      } else if (key.equals("inventoryCount") || key.equals("pahmaFieldLocVerbatim") || key.equals("pahmaEthnographicFileCodeList")) {
-        pahmaValues += "<" + key + ">" + value + "</" + key + ">";
-        pahma = true;
+      
+      if (key.equals("material")) {
+        commonValues += "<materialGroupList><materialGroup><" + key + ">" + value + "</" + key + "></materialGroup></materialGroupList>";
+      } else if (key.equals("responsibleDepartment")) {
+        commonValues += "<responsibleDepartments><" + key + ">" + value + "</" + key + "></responsibleDepartments>";
+      } else if (key.equals("assocPeople")) {
+        commonValues += "<assocPeopleGroupList><assocPeopleGroup><" + key + ">" + value + "</" + key + "></assocPeopleGroup></assocPeopleGroupList>";
+      } else if (key.equals("objectProductionPerson")) {
+        commonValues += "<objectProductionPersonGroupList><objectProductionPersonGroup><" + key + ">" + value + "</" + key + "></objectProductionPersonGroup></objectProductionPersonGroupList>";
+      } else if (key.equals("objectProductionPlace")) {
+        commonValues += "<objectProductionPlaceGroupList><objectProductionPlaceGroup><" + key + ">" + value + "</" + key + "></objectProductionPlaceGroup></objectProductionPlaceGroupList>";
+      } else if (key.equals("fieldCollector")) {
+        commonValues += "<fieldCollectors><" + key + ">" + value + "</" + key + "></fieldCollectors>";
+      } else if (key.equals("objectStatus")) {
+        commonValues += "<objectStatusList><" + key + ">" + value + "</" + key + "></objectStatusList>";
+      } else if (key.equals("contentPlace")) {
+        commonValues += "<contentPlaces><" + key + ">" + value + "</" + key + "></contentPlaces>";
+      } else if (key.equals("objectName")) {
+        commonValues += "<objectNameList><objectNameGroup><" + key + ">" + value + "</" + key + "></objectNameGroup></objectNameList>";
+      } else if (key.equals("briefDescription")) {
+        commonValues += "<briefDescriptions><" + key + ">" + value + "</" + key + "></briefDescriptions>";
+      } else if (key.equals("numberValue") || key.equals("numberType")) {
+        otherNumber += "<" + key + ">" + value + "</" + key + ">";
+        otherNumFlag = true;
+      } else if (key.equals("objectProductionDate")) {
+        commonValues += "<objectProductionDateGroupList><objectProductionDateGroup><dateDisplayDate>" + value + "</dateDisplayDate></objectProductionDateGroup></objectProductionDateGroupList>";
+      } else if (key.equals("contentDate")) {
+        commonValues += "<contentDateGroup><dateDisplayDate>" + value + "</dateDisplayDate></contentDateGroup>";
+      } else if (key.equals("fieldCollectionDateGroup")) {
+          commonValues += "<fieldCollectionDateGroup><dateDisplayDate>" + value + "</dateDisplayDate></fieldCollectionDateGroup>";
       } else {
         commonValues += "<" + key + ">" + value + "</" + key + ">";
       }
+      // } else if (key.equals("taxonomicIdentGroupList")) {
+        // ucbnhValues += "<taxonomicIdentGroup><" + key + ">" + value + "<" + key + "/><taxonomicIdentGroup/>";
+        // ucbnh = true;
+      // } else if (key.equals("materialGroup")) {
+      //   commonValues += "<materialGroupList><" + key + ">" + value + "<" + key + "/><materialGroupList/>";
+      // } else if (key.equals("inventoryCount") || key.equals("pahmaFieldLocVerbatim") || key.equals("pahmaEthnographicFileCodeList")) {
+      //   pahmaValues += "<" + key + ">" + value + "</" + key + ">";
+      //   pahma = true;
+      // } else {
+      //   commonValues += "<" + key + ">" + value + "</" + key + ">";
+      // }
+    }
+
+    if (otherNumFlag) {
+      otherNumber += "</otherNumber></otherNumberList>";
+      commonValues += otherNumber;
     }
 
     String tenant = "";
@@ -118,14 +160,14 @@ public class BulkObjectEditBatchJob extends  AbstractBatchJob {
     "<document name=\"collectionobjects\">" +
       "<ns2:collectionobjects_common " +
       "xmlns:ns2=\"http://collectionspace.org/services/collectionobject\" " +
-      "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" + 
+      "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
       commonValues +
     "</ns2:collectionobjects_common>";
 
     if (pahma) {
-      commonPayload += "<ns2:collectionobjects_pahma " + 
+      commonPayload += "<ns2:collectionobjects_pahma " +
                         "xmlns:ns2=\"http://collectionspace.org/services/collectionobject/local/pahma\" " +
-                        "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" + pahmaValues + 
+                        "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" + pahmaValues +
                         "</ns2:collectionobjects_pahma>";
     }
     if (ucbnh) {
@@ -141,19 +183,81 @@ public class BulkObjectEditBatchJob extends  AbstractBatchJob {
     return commonPayload;
   }
 
+  public String mergePayloads(String csid, PoxPayloadOut batchPayload) throws Exception {
+    // now we have the bytes for both t
+    HashMap<String, Element> elementList = new HashMap<String, Element>();
+    PoxPayloadOut collectionObjectPayload = findCollectionObjectByCsid(csid);
 
-  public int updateRecord(String csid, String payload) throws URISyntaxException {
-    int result = 0;
-
-    ResourceMap resource = getResourceMap();
-    NuxeoBasedResource collectionObjectResource = (NuxeoBasedResource) resource.get(CollectionObjectClient.SERVICE_NAME);
-
-    String response = new String (collectionObjectResource.update(getServiceContext(), resource, createUriInfo(), csid, payload));
-
-    if (response != "200") {
-      result = -1;
+    // Now we have a list of Elements that we can go thru and update
+    for (PayloadOutputPart candidatePart : batchPayload.getParts()) {
+      Element candidatePartElement = candidatePart.asElement();
+      elementList.put(candidatePartElement.getName(), candidatePartElement);
     }
 
+    for (String elem : elementList.keySet()) {
+
+      Element collectionObjectElement = collectionObjectPayload.getPart(elem).asElement();
+
+      Element batchElement = elementList.get(elem);
+
+
+//      collectionObjectPayload.addPart(); label, elementBody
+      for (Element batchElemChild : (List<Element>) batchElement.elements()) {
+        String childElemName = batchElemChild.getName(); // Now we need to find the matching element
+
+        if (childElemName == null) {continue;}
+
+
+//        Element collectionObjElement = collectionObjectElement.element("objectStatusList")
+        Element collectionObjElementList = collectionObjectElement.element(childElemName);
+
+//        if size of elements() below is 0, then it is non repeating, so just remove and set
+        for (Element objElem : (List<Element>) collectionObjElementList.elements()) {
+//          if elements is empty then its not a repeating list
+          // if the objElemName is in the list of singleFields...then dont add it but we can get to that later
+          batchElemChild.add(objElem.createCopy());
+        }
+
+        collectionObjectElement.remove(collectionObjElementList);
+        collectionObjectElement.add(batchElemChild.createCopy());
+//        collectionObjElement.add(batchElemChild.elements().get(0).createCopy());
+
+//        collectionObjectPayload.addPart(); label, elementBody
+//        PoxPayloadOut pLoad = collectionObjectPayload.addPart(childElemName, batchElemChild);
+
+//        collectionObjectElement
+
+//        collectionObjectElement.add(batchElemChild);
+//        public PayloadOutputPart addPart(String label, Element elementBody) {
+
+        int y = 0;
+
+      }
+
+      // now we have both elements. Merge uwu
+
+
+    }
+
+    
+
+    return collectionObjectPayload.asXML();
+    // use result.asXML to get a final form
+  }
+  
+  public int updateRecord(String csid, String payload) throws URISyntaxException, DocumentException {
+    PoxPayloadOut collectionObjectPayload = findCollectionObjectByCsid(csid);
+
+    int result = 0;
+
+    try {
+      ResourceMap resource = getResourceMap();
+      NuxeoBasedResource collectionObjectResource = (NuxeoBasedResource) resource.get(CollectionObjectClient.SERVICE_NAME);
+
+      String response = new String(collectionObjectResource.update(getServiceContext(), resource, createUriInfo(), csid, payload));
+    } catch (Exception e) {
+      result = -1;
+    }
     return result;
 
   }
